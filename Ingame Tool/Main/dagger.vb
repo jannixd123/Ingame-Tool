ec9515cac0d11b517aa186955adc8e227a1135af
jannixd123 <janni-damien@hotmail.com>
2016-05-31 18:48:15 +0200

    Revert "Design"
    This reverts commit ce7c3bc628ca8ffe8d4ffe70c9bafe2ce3f17305.

--- Ingame Tool/Main/dagger.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class dagger
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.Close()
-    End Sub
-End Class
